46aab379756dd0eb7942fe13482118f7050d6382
AxelDupraz <Axel.Dupraz@insa-rennes.fr>
2024-04-29 11:25:25 +0200

    path changes

--- server/plugins/fileFetch.ts
+++ server/plugins/fileFetch.ts
@@ -80,7 +80,7 @@ export default function fileFetch() {
 
      // Call fetchDataAndInsertIntoDatabase at regular intervals
      setInterval(() => {
-        const filePath: string = '.\\filtered_requests.log';
+        const filePath: string = './filtered_requests.log';
         //const filePath: string = '..\\ks\\serveurRequete\\filtered_requests.log';
         //const filePath: string = '..\\filtered_requests.log';
         fetchDataAndInsertIntoDatabase(filePath);
